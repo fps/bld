dbfcda1df628832642b1a9ca8afe675bf730281c
Florian Paul Schmidt <mista.tapas@gmx.net>
2012-08-03 00:35:22 +0200

    more simple

--- bld.lua
+++ bld.lua
@@ -8,39 +8,71 @@ function lift(x)
 end
 
 
-function cpp_closure(compiler, source)
-	command = compiler .. " " .. "-c" .. " " .. source
-	print("executing: ", command)
-	os.execute (command) 
-	return true
+function valevaleval(v)
+        if type(v) == "function" then
+                return valevaleval(v())
+        else
+                return v
+        end
 end
 
-function cpp(source)
-	local s = source
-	return function() 
-		return cpp_closure("g++", s) 
-	end 
+function valeval(v)
+        if type(v) == "function" then
+                return v()
+        else
+                return v
+        end
 end
 
-function dynlib_closure(name, tasks)
-	print ("building: ", name, " (dynlib)")
-	for i = 1, # tasks do 
-		tasks[i]() 
-	end 
-	return true
-end
-
-function dynlib(name, sources)
-	local s = sources()
-	local tasks = {}
-	for i = 1, #s do
-		print ("adding task: ", s[i])
-		local task = cpp(s[i])
-		table.insert(tasks, task) 
+function rule_closure(target, source, transformation, parameters, check)
+	if not (nil == check) then 
+		print ("-- check!")
+		if check(valeval(target), valeval(source)) == true  then
+			print ("-- check!!!")
+			return 
+		end
 	end
-	return function ()
-		return dynlib_closure(name, tasks) 
-	end 
+
+	print ("-- building: ", valeval(target), "from: ", valeval(source))
+        return transformation(valeval(target), valeval(source), valeval(parameters))
+end
+
+function rule(target, sources, transformation, parameters, check)
+	print("-- adding rule") 
+	local t = target
+	local s = sources
+	local tr = transformation
+	local p = parameters
+	local c = check
+        return function()
+                        return rule_closure(t, s, tr, p, c)
+        end
+end
+
+
+
+function shell_cmd(target, source, cmd, parameters)
+	local t = target
+	local s = source
+	local c = cmd
+	local p = parameters
+	local f = function() 
+		print("-- running: ", cmd)
+		os.execute(c) 
+	end
+        return rule(t, s, f, {  }, function(t, s) return false end)
+end
+
+function pkg(name, version, rules)
+	local n = name
+	local v = version
+	
+	return function()
+		print("-- project: ", name, " version: ", version)
+		for i,j in pairs(rules) do
+			j()
+		end
+	end
 end
 
 function glob_closure(globble)
@@ -53,16 +85,19 @@ function glob(globble)
 	end 
 end
 
-function configure() 
-	return { dynlib("foo", glob("*.c")) } end
 
-if arg[1] == "configure" then
-	local t = configure()
-	local out = assert(io.open("out.bld", "wb"))
-	out:write(DataDumper(t))
-	assert(out:close()) end
-
-if arg[1] == "build" then
-	local t = dofile("out.bld")
-	t[1]() end
-
+function bld(args, configure)
+	if args[1] == "configure" then
+		local t = { configure() }
+		local out = assert(io.open("out.bld", "wb"))
+		out:write(DataDumper(t))
+		assert(out:close()) 
+	end
+	
+	if args[1] == "build" then
+		local t = dofile("out.bld")
+		for i,j in pairs(t) do
+			j() 
+		end
+	end
+end	
