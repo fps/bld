aef7156aa9b757e3fbe698d21afdc397c3028525
Florian Paul Schmidt <mista.tapas@gmx.net>
2012-08-04 11:30:43 +0200

    check

--- bld.lua
+++ bld.lua
@@ -26,9 +26,9 @@ end
 
 function rule_closure(target, source, transformation, parameters, check)
 	if not (nil == check) then 
-		print ("-- check!")
+		--  print ("-- check!")
 		if check(valeval(target), valeval(source)) == true  then
-			print ("-- check!!!")
+			print ("-- done")
 			return 
 		end
 	end
@@ -39,16 +39,37 @@ end
 
 function rule(target, sources, transformation, parameters, check)
 	print("-- adding rule") 
+
 	local t = target
 	local s = sources
 	local tr = transformation
 	local p = parameters
 	local c = check
+
         return function()
                         return rule_closure(t, s, tr, p, c)
         end
 end
 
+function exists(n)
+	local f = io.open(n)
+	if f then
+		io.close(f)
+	end
+	return f == nil
+end
+
+function file(target, source)
+	local t = target
+	return function(target) 
+		if exists(valeval(target)) then
+			return true
+		end
+
+		print ("-- -- build")
+		return false
+	end
+end
 
 function pkg_config(pkg_name)
 	local cflags = "-I /usr"
@@ -57,6 +78,22 @@ function pkg_config(pkg_name)
 	return function()
 		return { cflags, ldflags }
 	end
+end
+
+function cc(target, source, parameters)
+	local t = target
+	local s = source
+	local p = parameters
+	local f = function(target, source, parameters)
+		local cmd = "g++ -o " .. valeval(t) .. " -c " .. valeval(s)
+		print(cmd)
+		os.execute(cmd)
+	end
+	return rule(t, s, f, { }, file(t, s))
+end
+
+function exe(target, source, parameters)
+
 end
 
 function shell_cmd(target, source, cmd, parameters)
@@ -68,7 +105,7 @@ function shell_cmd(target, source, cmd, parameters)
 		print("-- running: ", cmd)
 		os.execute(c) 
 	end
-        return rule(t, s, f, {  }, function(t, s) return false end)
+        return rule(t, s, f, {  }, file(t,s))
 end
 
 function pkg(name, version, rules)
--- test.lua
+++ test.lua
@@ -1,7 +1,9 @@
 require "bld"
 
 configure = function() 
-	local foo1 = shell_cmd("foo1", "bar1", "echo", {})
+	local compiler = cc("foo.o", "foo.c", {})	
+
+	local foo1 = shell_cmd("foo1", compiler, "echo", {})
 	local foo2 = shell_cmd("foo2", "bar1", "echo", {})
 
 	return pkg("foo", "0.1", { foo1, foo2 }) 
